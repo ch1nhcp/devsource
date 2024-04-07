2b1f48710085f7d7366824e0589fba888ded9930
chinhcp <chinhcpdev@gmail.com>
2024-04-07 17:10:28 +0700

    initial commits, vite + react-router-dom + tailwind starter kit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
